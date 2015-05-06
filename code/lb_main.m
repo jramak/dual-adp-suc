674c70ce44fb4d55c8c7600efadd5cff5a73a6f6
Jagdish Ramakrishnan <jramakrishn2@wisc.edu>
2015-05-06 18:58:41 +0000

    updated assert statement in lb_main.m
    git-svn-id: svn+ssh://svn1.discovery.wisc.edu/data/svndata/optimization/jramakrishnan/jim_master_code@47 ac8c36bb-b2a3-499c-85e3-34758e65ab6a

--- code/lb_main.m
+++ code/lb_main.m
@@ -18,5 +18,5 @@ numGens = length(Lu) - 2; % number of generators not including buy/sell
 % modelstatus = 8 means integer solution found
 % make sure less than 0.01% optimality gap for all runs
 irgdx('lb_per_info.gdx');
-assert(all(abs(modelstatus - 8) <= 0.01));
+assert(all((abs(modelstatus - 8) <= 0.01) | (abs(modelstatus - 1) <= 0.01)));
 assert(all(optcr <= 0.0002));
