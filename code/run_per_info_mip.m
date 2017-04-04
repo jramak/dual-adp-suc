83a96ffb6bca40cf10380754206e56459a6199c7
Jagdish Ramakrishnan <jramakrishn2@wisc.edu>
2017-04-04 17:00:49 +0000

    added new file single run per info gms.
    git-svn-id: svn+ssh://svn1.discovery.wisc.edu/data/svndata/optimization/jramakrishnan/jim_master_code@81 ac8c36bb-b2a3-499c-85e3-34758e65ab6a

---
+++ code/run_per_info_mip.m
@@ -0,0 +1,20 @@
+function run_per_info_mip(filename, num_scenarios)
+
+irgdx(filename);
+Lu = Lu;
+numGens = length(Lu) - 2; % number of generators not including buy/sell
+
+parfor i = 1:num_scenarios
+  [status,result]=system(['gams per_info_mip --probdata=',...
+                          filename,' --subprob=',num2str(numGens+2),...
+                          ' --s=', num2str(i),...
+                          ' mip=cplex o=/dev/null lo=0']);
+  % modelstatus = 8 means integer solution found
+  % make sure less than 0.01% optimality gap for all runs
+  irgdx(['lb_per_info_', num2str(i), '.gdx']);
+  assert(length(modelstatus) == 100);
+  assert(all((abs(modelstatus - 8) <= 0.01) | (abs(modelstatus - 1) <= 0.01)));
+  assert(all(optcr <= 0.1));
+end
+
+
