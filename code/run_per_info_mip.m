3357153e5b864612a11b6932a302b2969f79a5a4
Jagdish Ramakrishnan <jramakrishn2@wisc.edu>
2017-04-05 17:30:28 +0000

    updated so there is no bug running.
    git-svn-id: svn+ssh://svn1.discovery.wisc.edu/data/svndata/optimization/jramakrishnan/jim_master_code@86 ac8c36bb-b2a3-499c-85e3-34758e65ab6a

--- code/run_per_info_mip.m
+++ code/run_per_info_mip.m
@@ -13,14 +13,19 @@ parfor i = 1:num_scenarios
                           ' --scenario=', num2str(i),...
                           ' mip=cplex o=/dev/null lo=0']);
   %result
-  % modelstatus = 8 means integer solution found
-  % make sure less than 0.01% optimality gap for all runs
-  irgdx(['lb_per_info_', num2str(i), '.gdx']);
-  assert(length(modelstatus) == 100);
-  assert(all((abs(modelstatus - 8) <= 0.01) | (abs(modelstatus - 1) <= 0.01)));
-  assert(all(optcr <= 0.1));
   scenario_times(i) = toc(tind);
 end
 tot_time = toc(ttot);
 
-save(['per_info_times_',filename]);
+obj_lb = nan(num_scenarios, 1);
+for i = 1:num_scenarios
+  % modelstatus = 8 means integer solution found
+  % make sure less than 0.01% optimality gap for all runs
+  irgdx(['lb_per_info_', num2str(i), '.gdx']);
+  assert(length(modelstatus) == 1);
+  assert(all((abs(modelstatus - 8) <= 0.01) | (abs(modelstatus - 1) <= 0.01)));
+  assert(all(optcr <= 0.1));
+  obj_lb(i) = objEst_lb;
+end
+
+save(['per_info_',filename]);
