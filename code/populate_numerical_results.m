26bdb3cfd07aa42882e911d5a7b45f3b85ab1cd9
Jagdish Ramakrishnan <jramakrishn2@wisc.edu>
2017-04-06 19:05:32 +0000

    file for populating results.
    git-svn-id: svn+ssh://svn1.discovery.wisc.edu/data/svndata/optimization/jramakrishnan/jim_master_code@87 ac8c36bb-b2a3-499c-85e3-34758e65ab6a

---
+++ code/populate_numerical_results.m
@@ -0,0 +1,55 @@
+% 15 gen problem
+mat_res = dir('./gen15/lb_bounds_final/lb_*');
+num = length(mat_res);
+m = [4, 6, 8];
+sig = [15, 20, 25];
+n = 100; % number of scenarios
+
+lb_bounds_15 = nan(num, 7);
+solve_times_15 = nan(num, 6);
+for idx_i = 1:num
+  filename = mat_res(idx_i).name;
+  cd(['gen15/lb_bounds_final/',filename]);
+  m_v = str2num(filename(9));
+  sig_v = str2num(filename(13:14));
+  m_idx = find(m == m_v);
+  sig_idx = find(sig == sig_v);
+  lin_idx = 3 * (m_idx - 1) + sig_idx;
+  lb_bounds_15(lin_idx, 1) = m_v * 0.1;
+  lb_bounds_15(lin_idx, 2) = sig_v * 0.01;
+  lb_bounds_15(lin_idx, 3) = 15;
+  solve_times_15(lin_idx, 1) = m_v * 0.1;
+  solve_times_15(lin_idx, 2) = sig_v * 0.01;
+  solve_times_15(lin_idx, 3) = 15;
+  
+  load_tmp = dir('max_Lg_r_uc*.mat');
+  if ~(length(load_tmp) == 0)
+    assert(length(load_tmp) == 1);
+    load(load_tmp.name);
+    lb_bounds_15(lin_idx, 4) = best_cost / 1e6;
+    solve_times_15(lin_idx, 4) = tot_time / 60; 
+  end  
+
+  load_tmp = dir('max_Lg_r_d_uc*.mat');
+  if ~(length(load_tmp) == 0)
+    assert(length(load_tmp) == 1);
+    load(load_tmp.name);
+    lb_bounds_15(lin_idx, 5) = best_cost / 1e6;
+    solve_times_15(lin_idx, 5) = tot_time / 60;
+  end
+
+  load_tmp = dir('per_info_uc*.mat');
+  if ~(length(load_tmp) == 0)
+    assert(length(load_tmp) == 1);
+    load(load_tmp.name);
+    lb_bounds_15(lin_idx, 6) = mean(obj_lb) / 1e6;
+    lb_bounds_15(lin_idx, 7) = 1.96 * std(obj_lb) / (1e6 * sqrt(n)); 
+    solve_times_15(lin_idx, 6) = tot_time / 60;
+  end 
+  cd ..
+  cd ..
+  cd ..
+end
+  
+lb_bounds_15
+solve_times_15
--- code/startup.m
+++ code/startup.m
@@ -1,3 +1,4 @@
 path('/mnt/ws/progs/CPLEX_Studio125/cplex/matlab',path);
 setenv('PATH',[getenv('PATH') ':/mnt/ws/progs/CPLEX_Studio125/cplex/bin']);
 addpath '/mnt/ws/progs/gams/gams24.3_linux_x64_64_sfx';
+addpath '~/latexTable/';
