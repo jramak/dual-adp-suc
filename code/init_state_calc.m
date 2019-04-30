dc2fb2010fbbd6f904b6a5092d41389130767409
Jagdish Ramakrishnan <jagdish.ram@gmail.com>
2019-04-29 19:17:08 -0700

    add parenthesis in if/else statements.

--- code/init_state_calc.m
+++ code/init_state_calc.m
@@ -11,13 +11,13 @@ gen_state = ones(numGens, 1) * (Lu + Ld); % initial start state
 
 for i = 1:numGens
     for t = 1:T
-        if gen_state(i) < Lu(i)
+        if (gen_state(i) < Lu(i))
             gen_state(i) = gen_state(i) + 1;
         elseif ((gen_state(i) == Lu(i)) && (u_s(i, t) == 0))
             gen_state(i) = gen_state(i) + 1;
         elseif ((Lu(i) < gen_state(i)) && (gen_state(i) < (Lu(i) + Ld(i))))
             gen_state(i) = gen_state(i) + 1;
-        elseif (gen_state(i) == (Lu(i) + Ld(i))) && (u_s(i, t) == 1)
+        elseif ((gen_state(i) == (Lu(i) + Ld(i))) && (u_s(i, t) == 1))
             gen_state(i) = 1;
         end
     end
