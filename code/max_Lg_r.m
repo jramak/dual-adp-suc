6e3f08c381dca49ecd38a760faafe600985d83bd
Jagdish Ramakrishnan <jagdish.ram@gmail.com>
2019-05-03 17:33:37 -0700

    add gen_state and z_prev to max_Lg_r.m

--- code/max_Lg_r.m
+++ code/max_Lg_r.m
@@ -1,11 +1,11 @@
 function [best_cost,best_idx,opt_cost_vec,time,tot_time,L_g_cost,...
-    z_sol_avg] = max_Lg_r(filename,maxIters,rho0,stepVec)
+    z_sol_avg] = max_Lg_r(filename,maxIters,rho0,stepVec,gen_state,z_prev)
 
 % load in from data file
 irgdx(filename);
 
 % need to assign data for the sake of the parfor loop
-numPts = numPts; 
+numPts = numPts;
 q = q;
 c = c;
 c_bar = c_bar;
@@ -66,7 +66,7 @@ grad = nan(size(lambda)); % stochastic subgradient
 % keep track of best cost information when optimizating Lagrangian
 best_idx = 1;
 best_cost = -inf;
-best_lambda = nan(size(lambda)); 
+best_lambda = nan(size(lambda));
 opt_cost_vec = nan(maxIters,1);
 best_L_g_cost = nan(numGens+2,1);
 best_grad = nan(size(lambda));
@@ -100,7 +100,8 @@ for k = 1:maxIters
         tind = tic;
         [L_g_cost(i),z_sol_avg(:,i),V1{i},uGen1{i},V2{i},uGen2{i}] = ...
             Lg_r(q_min(i),q_max(i),c_bar(i),h_bar(i),Lu(i),Ld(i),lambda,...
-            T,Rd(i),Ru(i),nPts,Pow(:,i),Feval(:,i),Pstep(i));
+            T,Rd(i),Ru(i),nPts,Pow(:,i),Feval(:,i),Pstep(i),gen_state(i),...
+            z_prev(i));
         time(i) = toc(tind);
     end
     tind = tic;
@@ -111,33 +112,33 @@ for k = 1:maxIters
         h_bar(numGens+1),Lu(numGens+1),Ld(numGens+1),lambda,T);
     time(numGens+1) = toc(tind);
 
-    tind = tic; 
+    tind = tic;
     % for sell generator
     [L_g_cost(numGens+2),z_sol_avg(:,numGens+2),V(2,:,:),uGen(2,:,:)]...
         = Lg(q(numGens+2,1:numPts(numGens+2))',...
         c(numGens+2,1:numPts(numGens+2))',c_bar(numGens+2),...
         h_bar(numGens+2),Lu(numGens+2),Ld(numGens+2),-lambda,T);
     z_sol_avg(:,numGens+2) = -1*z_sol_avg(:,numGens+2);
-    time(numGens+2) = toc(tind); 
-    
+    time(numGens+2) = toc(tind);
+
     opt_cost_vec(k) = sum(L_g_cost) + sum(lambda.*(x_s*p_s));
-    
+
     if (opt_cost_vec(k) > best_cost)
         best_cost = opt_cost_vec(k);
         best_idx = k;
-        best_lambda = lambda; 
+        best_lambda = lambda;
         best_V1 = V1;
         best_uGen1 = uGen1;
         best_V2 = V2;
         best_uGen2 = uGen2;
-        best_V = V; 
+        best_V = V;
         best_uGen = uGen;
         best_L_g_cost = L_g_cost;
         best_grad = grad;
     end
-    
+
     grad = x_s*p_s - sum(z_sol_avg,2);
-    lambda = lambda + stepVec(k).*rho0.*grad;  
+    lambda = lambda + stepVec(k).*rho0.*grad;
 %     % some other possible choices for subgradient step
 %     lambda = lambda + alpha^k.*rho0.*grad;
 %     lambda = lambda + (1/k).*rho0.*grad;
