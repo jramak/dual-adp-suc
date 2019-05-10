f173c0360664b01f2ba52a8de5e89f4ffb032d6e
Jagdish Ramakrishnan <jagdish.ram@gmail.com>
2019-05-10 14:42:22 -0700

    add stay_on, stay_off to init_state_calc.m

--- code/init_state_calc.m
+++ code/init_state_calc.m
@@ -1,4 +1,4 @@
-function [gen_state, z_prev, u_prev] = init_state_calc(filename)
+function [gen_state, z_prev, u_prev, stay_on, stay_off] = init_state_calc(filename)
 
 T = 72;
 
@@ -9,7 +9,9 @@ numGens = length(Lu) - 2; % number of generators not including buy/sell
 
 z_prev = z_s(1:numGens, T);
 u_prev = u_s(1:numGens, T);
-gen_state = Lu + Ld; % initial start state
+gen_state = ones(numGens+2, 1) .* Lu + Ld; % initial start state
+stay_on = zeros(numGens+2, 1);
+stay_off = zeros(numGens+2, 1);
 
 for i = 1:numGens
     for t = 1:T
@@ -22,5 +24,14 @@ for i = 1:numGens
         elseif ((gen_state(i) == (Lu(i) + Ld(i))) && (u_s(i, t) == 1))
             gen_state(i) = 1;
         end
+    end
+end
+
+for i = 1:numGens
+    if gen_state(i) < Lu(i)
+        stay_on(i) = Lu(i) - gen_state(i)
+    end
+    if (gen_state(i) > Lu(i)) && (gen_state(i) < (Lu(i) + Ld(i)))
+        stay_off(i) = (Lu(i) + Ld(i)) - gen_state(i)
     end
 end
