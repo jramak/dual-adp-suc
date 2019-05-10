f1dc36ba3c4ca442295cf413bb35bb4ed028f337
Jagdish Ramakrishnan <jagdish.ram@gmail.com>
2019-05-10 15:26:25 -0700

    update upper bound with initial state.

--- code/compute_ub_fv_mip.m
+++ code/compute_ub_fv_mip.m
@@ -10,8 +10,8 @@ irgdx(filename,'D_s_ub','D_s_idx_ub');
 
 % solution for every sample path = numDemands x T x numsamplepaths
 % store solution for every sample path
-u_sol_mat_ub = nan(T,numGens,numSamples); 
-z_sol_mat_ub = nan(T,numGens+2,numSamples); 
+u_sol_mat_ub = nan(T,numGens,numSamples);
+z_sol_mat_ub = nan(T,numGens+2,numSamples);
 
 % store ub cost and optimization time
 ub_cost = nan(T,numSamples);
@@ -23,30 +23,31 @@ modelstat_mat = nan(T,numSamples);
 for i = 1:numGens
     tmpV1 = V1{i};
     tmpV1(isinf(tmpV1)) = 1e50;
-    V1{i} = tmpV1; 
-    
+    V1{i} = tmpV1;
+
     tmpV2 = V2{i};
     tmpV2(isinf(tmpV2)) = 1e50;
-    V2{i} = tmpV2; 
+    V2{i} = tmpV2;
 end
 
 myeps = 0.01;
 myeps2 = 0.1;
 
-tic; 
+tic;
 for k = 1:numSamples
     % initialize all states to be Lu+Ld
-    states = (Lu(1:numGens)+Ld(1:numGens)).*ones(numGens,1); 
-    u_prev = zeros(numGens,1);
-    z_prev = zeros(numGens,1);
+    %states = (Lu(1:numGens)+Ld(1:numGens)).*ones(numGens,1);
+    states = gen_state;
+    u_prev = u_init;
+    z_prev = z_init;
     Voff = zeros(numGens,1);
     Von = zeros(numGens,nPts);
-    
+
     for t = 1:T
         % reset lower and upper bounds on generator on/off decisions
         u_fx_lo = zeros(numGens,1);
         u_fx_hi = ones(numGens,1);
-        
+
         % compute future value function for use in optimization
         % and also set gen on/off constraint
         for i = 1:numGens
@@ -54,7 +55,7 @@ for k = 1:numSamples
                 % generator must stay on in next period
                 u_fx_lo(i) = 1;
                 u_fx_hi(i) = 1;
-                
+
                 tmpV = V1{i};
                 Von(i,:) = tmpV(states(i)+1,:,t+1);
                 Voff(i) = 0; % optimization should not choose this
@@ -67,7 +68,7 @@ for k = 1:numSamples
                     (states(i) < (Lu(i)+Ld(i)-myeps)))
                 u_fx_lo(i) = 0;
                 u_fx_hi(i) = 0;
-                
+
                 Von(i,:) = 0; % optimization should not choose this
                 tmpV = V2{i};
                 Voff(i) = tmpV(states(i)+1-Lu(i),t+1);
@@ -80,7 +81,7 @@ for k = 1:numSamples
                 error('something wrong with states(i)');
             end
         end
-        
+
         % store data and run optimization
         iwgdx('ubdata.gdx','u_prev',[u_prev;0;0],'z_prev',[z_prev;0;0],...
             'u_fx_lo',[u_fx_lo;1;1],'u_fx_hi',[u_fx_hi;1;1],'Von',Von,...
@@ -88,26 +89,26 @@ for k = 1:numSamples
         [status,result]=system(['gams ub_fv_mip --probdata=',...
             filename,' --subprob=',num2str(numGens+2),' --nPts=',num2str(nPts),' mip=cplex lo=3']);
         irgdx('ubout.gdx');
-        
+
         assert(abs(optcr) <= myeps2);
         assert((abs(modelstatus - 8) <= myeps) | (abs(modelstatus - 1) <= myeps));
-        
+
         % store results from optimization
         ub_cost(t,k) = tot_cost(D_s_idx_ub(t,k));
-        ub_time(t,k) = time; 
+        ub_time(t,k) = time;
         optca_mat(t,k) = optca;
         modelstat_mat(t,k) = modelstatus;
         u_prev = u_l(1:numGens);
         u_sol_mat_ub(t,:,k) = u_l(1:numGens);
-        
+
         flag_rel = logical(u_l(1:numGens));
-        
+
         % set previous power level of generators
         z_prev(~flag_rel) = 0;
         z_prev(flag_rel) = z_l([flag_rel;false;false],D_s_idx_ub(t,k));
-        z_sol_mat_ub(t,:,k) = z_l(:,D_s_idx_ub(t,k)); 
-        
-        % do a state update for each generator    
+        z_sol_mat_ub(t,:,k) = z_l(:,D_s_idx_ub(t,k));
+
+        % do a state update for each generator
         for i = 1:numGens
             if (states(i) < (Lu(i) - myeps))
                 % check to make sure generator stayed on
@@ -123,7 +124,7 @@ for k = 1:numSamples
                     (states(i) < (Lu(i)+Ld(i)-myeps)))
                 % generator must have stayed off
                 assert(abs(u_l(i)) <= myeps);
-                states(i) = states(i) + 1; 
+                states(i) = states(i) + 1;
             elseif (abs(states(i) - (Lu(i)+Ld(i))) <= myeps)
                 if (abs(u_l(i) - 1) <= myeps)
                     states(i) = 1;
@@ -133,7 +134,7 @@ for k = 1:numSamples
             else
                 error('something wrong with states(i)');
             end
-        end       
+        end
     end
 end
 tot_run_time = toc;
